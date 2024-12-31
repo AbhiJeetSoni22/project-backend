6d27bd187c3720344add5ebc4185c6423a8df80d
AbhiJeetSoni22 <AbhiJeetSoni22>
2024-12-31 17:18:46 +0530

    adding util files

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,7 @@ import { DB_NAME } from '../constants.js';
 const connectDB = async ()=>{
     try {
        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n mongodb connect by abhi :: ${connectionInstance.connection.host}`)
+        console.log(`\n mongodb connect again :: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("mongodb connection error : ",error)
         process.exit(1)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 // required('dotenv').config({path:'./env'})
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-
+import {app} from './app.js';
 import connectDB from './db/db.js';
 dotenv.config({  
       path:'./env'
 })
 connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000 , ()=>{
+    console.log(`listening on ${process.env.PORT}`)
+  })
+}).catch((err) => {
+ console.log("Faild mongodb connection !!!!",err)   
+});
 
 
 
